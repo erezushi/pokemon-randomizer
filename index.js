ed7339eff9cd19e6bfead5e638bc6c8b56f5f082
Dylan Stankievech <dylan.stank@gmail.com>
2018-11-28 20:32:18 -0700

    Read pokemon file

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
 
 const package = require('./package.json');
 const program = require('commander');
+const fsp = require('fs').promises;
 
+let pokemon = {};
+const pokemonFile = 'pokemon.json';
+loadPokemon()
+    .then(() => {
+        console.log('loaded pokemon');
+    })
+    .catch((err) => {
+        console.error('errored at top level!');
+        console.error(err);
+        process.exit(-1);
+    });
 
-program
+async function loadPokemon() {
+    try {
+        const pokemonString = await fsp.readFile(pokemonFile, 'utf8');
+        pokemon = JSON.parse(pokemonString);
+    } catch (error) {
+        console.error('failed to read', pokemonFile);
+        console.error(error);
+        process.exit(-1);
+    }
+}
+
+async function parseInput() {
+    program
     .version(package.version)
     .option('-n --number <n>', 'Number of Pokemon')
     .parse(process.argv);
-
-
-console.log('you chose', program.number , 'pokemon');
+}
