80c3fe547a95fbc28170d6c941dd78cc47e9efd2
Dylan Stankievech <dylan.stank@gmail.com>
2018-12-05 19:55:29 -0700

    Unit tests for filtering and picking pokemon

--- test/picker.test.js
+++ test/picker.test.js
@@ -2,6 +2,9 @@
 
 const picker = require('../lib/picker');
 const expect = require('chai').expect;
+const _ = require('lodash');
+const Chance = require('chance');
+const chance = new Chance();
 
 describe('getFilteredPokemon', async function () {
     it('should handle empty object options', async function () {
@@ -22,5 +25,34 @@ describe('getFilteredPokemon', async function () {
     it('should handle empty array options', async function () {
         const result = await picker.getFilteredPokemon([]);
         expect(result).to.exist;
+    });
+
+    it('should filter evolved of pokemon', async function () {
+        const options = {
+            evolved: true
+        };
+
+        const result = await picker.getFilteredPokemon(options);
+        _.forEach(result, (poke) => expect(poke.evolveTo).to.be.undefined);
+    });
+});
+
+describe('pickRandomPokemon', async function () {
+    it('should return expected number of pokemon', async function () {
+        const options = {
+            number: chance.integer({ min: 1, max: 100 })
+        };
+
+        const result = await picker.pickRandomPokemon(options);
+        expect(result.length).to.be.eq(options.number);
+    });
+
+    it('should return empty array for 0 pokemon', async function () {
+        const options = {
+            number: 0
+        };
+
+        const result = await picker.pickRandomPokemon(options);
+        expect(result.length).to.be.eq(options.number);
     });
 });
