43f3fee9e5423dbd9518f897bb5a7accb49fdce4
Dylan Stankievech <dylan.stank@gmail.com>
2018-12-27 16:52:30 -0700

    Added data loading tests

---
+++ test/data.test.js
@@ -0,0 +1,106 @@
+'ust strict';
+
+const data = require('../lib/data');
+const expect = require('chai').expect;
+const Chance = require('chance');
+const chance = new Chance();
+const _ = require('lodash');
+const proxyquire = require('proxyquire');
+
+describe('data', async function () {
+    let fakeData;
+    let fileContents;
+
+    beforeEach(async function () {
+        fakeData = proxyquire('../lib/data', {
+            'fs': {
+                promises: {
+                    readFile: async () => fileContents
+                }
+            }
+        });
+        fakeData.clearCache();
+    });
+
+    describe('stubbed data', async function () {
+        describe('getPokemon', async function () {
+            it('should parse file JSON', async function () {
+                const key = chance.string();
+                const value = chance.string();
+                fileContents = `{ "${key}": "${value}" }`;
+                const results = await fakeData.getPokemon();
+                expect(results[`${key}`]).to.be.eq(value);
+            });
+    
+            it('should return cached version', async function () {
+                const value1 = chance.string();
+                const value2 = chance.string();
+                fileContents = `{ "thing": "${value1}" }`;
+                await fakeData.getPokemon();
+                fileContents = `{ "thing": "${value2}" }`;
+                const results = await fakeData.getPokemon();
+                expect(results.thing).to.be.eq(value1);
+            });
+        });
+    
+        describe('getTypes', async function () {
+            it('should parse file JSON', async function () {
+                const key = chance.string();
+                const value = chance.string();
+                fileContents = `{ "${key}": "${value}" }`;
+                const results = await fakeData.getTypes();
+                expect(results[`${key}`]).to.be.eq(value);
+            });
+    
+            it('should return cached version', async function () {
+                const value1 = chance.string();
+                const value2 = chance.string();
+                fileContents = `{ "thing": "${value1}" }`;
+                await fakeData.getTypes();
+                fileContents = `{ "thing": "${value2}" }`;
+                const results = await fakeData.getTypes();
+                expect(results.thing).to.be.eq(value1);
+            });
+        });
+
+        describe('clearCache', async function () {
+            it('should return new file contents', async function () {
+                const value1 = chance.string();
+                const value2 = chance.string();
+                fileContents = `{ "thing": "${value1}" }`;
+                await fakeData.getPokemon();
+                await fakeData.clearCache();
+                fileContents = `{ "thing": "${value2}" }`;
+                const results = await fakeData.getPokemon();
+                expect(results.thing).to.be.eq(value2);
+            });
+        });
+    });
+
+    describe('real data', async function () {
+        beforeEach(async function () {
+            data.clearCache();
+        });
+
+        describe('getPokemon', async function () {
+            it('should return expected number of pokemon', async function () {
+                const results = await data.getPokemon();
+                expect(Object.keys(results).length).to.be.eq(129);
+            });
+
+            it('should return names for all pokemon', async function () {
+                const results = await data.getPokemon();
+                _.forEach(results, (result, key) => {
+                    expect(result.name).to.exist;
+                });
+            });
+
+            it('should return pikachu', async function () {
+                const results = await data.getPokemon();
+                const pikachu = results['25'];
+                expect(pikachu).to.exist;
+                expect(pikachu.name).to.be.eq('Pikachu');
+            });
+        });
+    });
+});
