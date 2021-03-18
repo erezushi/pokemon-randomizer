2633aaeca78184723d3903ad46e969fba8cd622c
Dylan Stankievech <dylan.stank@gmail.com>
2021-03-17 22:59:04 -0600

    Import pokemon and types from json directly

--- src/data.ts
+++ src/data.ts
@@ -4,33 +4,13 @@ import * as fsp from 'fs/promises';
 import * as constants from './constants';
 import * as path from 'path';
 import * as types from './types';
-
-let POKEMON: types.PokemonMap = {};
-let TYPES: types.TypeMap = {};
-
-export const clearCache = async () => {
-    POKEMON = {};
-    TYPES = {};
-};
+import POKEMON from './data/pokemon.json';
+import TYPES from './data/types.json';
 
 export const getPokemon = async (): Promise<types.PokemonMap> => {
-    if (Object.keys(POKEMON).length > 0) {
-        return POKEMON;
-    }
-
-    const filePath = path.join(__dirname, '..', constants.POKEMON_FILE);
-    const pokemonString = await fsp.readFile(filePath, 'utf8');
-    POKEMON = JSON.parse(pokemonString);
-    return POKEMON;
+    return (POKEMON as unknown) as types.PokemonMap;
 };
 
 export const getTypes = async (): Promise<types.TypeMap> => {
-    if (Object.keys(TYPES).length > 0) {
-        return TYPES;
-    }
-
-    const filePath = path.join(__dirname, '..', constants.TYPES_FILE);
-    const typesString = await fsp.readFile(filePath, 'utf8');
-    TYPES = JSON.parse(typesString);
-    return TYPES;
+    return (TYPES as unknown) as types.TypeMap;
 };
--- test/data.test.ts
+++ test/data.test.ts
@@ -10,97 +10,24 @@ import proxyquire from 'proxyquire';
 const chance = new Chance();
 
 describe('data', async function () {
-    let fakeData;
-    let fileContents;
-
-    beforeEach(async function () {
-        fakeData = proxyquire('../src/data', {
-            'fs/promises': {
-                readFile: async () => fileContents,
-            }
+    describe('getPokemon', async function () {
+        it('should return expected number of pokemon', async function () {
+            const results = await data.getPokemon();
+            expect(Object.keys(results).length).to.be.eq(151);
         });
-        fakeData.clearCache();
-    });
 
-    describe('stubbed data', async function () {
-        describe('getPokemon', async function () {
-            it('should parse file JSON', async function () {
-                const key = chance.string();
-                const value = chance.string();
-                fileContents = `{ "${key}": "${value}" }`;
-                const results = await fakeData.getPokemon();
-                expect(results[`${key}`]).to.be.eq(value);
-            });
-    
-            it('should return cached version', async function () {
-                const value1 = chance.string();
-                const value2 = chance.string();
-                fileContents = `{ "thing": "${value1}" }`;
-                await fakeData.getPokemon();
-                fileContents = `{ "thing": "${value2}" }`;
-                const results = await fakeData.getPokemon();
-                expect(results.thing).to.be.eq(value1);
-            });
-        });
-    
-        describe('getTypes', async function () {
-            it('should parse file JSON', async function () {
-                const key = chance.string();
-                const value = chance.string();
-                fileContents = `{ "${key}": "${value}" }`;
-                const results = await fakeData.getTypes();
-                expect(results[`${key}`]).to.be.eq(value);
-            });
-    
-            it('should return cached version', async function () {
-                const value1 = chance.string();
-                const value2 = chance.string();
-                fileContents = `{ "thing": "${value1}" }`;
-                await fakeData.getTypes();
-                fileContents = `{ "thing": "${value2}" }`;
-                const results = await fakeData.getTypes();
-                expect(results.thing).to.be.eq(value1);
+        it('should return names for all pokemon', async function () {
+            const results = await data.getPokemon();
+            _.forEach(results, (result, key) => {
+                expect(result.name).to.exist;
             });
         });
 
-        describe('clearCache', async function () {
-            it('should return new file contents', async function () {
-                const value1 = chance.string();
-                const value2 = chance.string();
-                fileContents = `{ "thing": "${value1}" }`;
-                await fakeData.getPokemon();
-                await fakeData.clearCache();
-                fileContents = `{ "thing": "${value2}" }`;
-                const results = await fakeData.getPokemon();
-                expect(results.thing).to.be.eq(value2);
-            });
-        });
-    });
-
-    describe('real data', async function () {
-        beforeEach(async function () {
-            data.clearCache();
-        });
-
-        describe('getPokemon', async function () {
-            it('should return expected number of pokemon', async function () {
-                const results = await data.getPokemon();
-                expect(Object.keys(results).length).to.be.eq(151);
-            });
-
-            it('should return names for all pokemon', async function () {
-                const results = await data.getPokemon();
-                _.forEach(results, (result, key) => {
-                    expect(result.name).to.exist;
-                });
-            });
-
-            it('should return pikachu', async function () {
-                const results = await data.getPokemon();
-                const pikachu = results['25'];
-                expect(pikachu).to.exist;
-                expect(pikachu.name).to.be.eq('Pikachu');
-            });
+        it('should return pikachu', async function () {
+            const results = await data.getPokemon();
+            const pikachu = results['25'];
+            expect(pikachu).to.exist;
+            expect(pikachu.name).to.be.eq('Pikachu');
         });
     });
 });
