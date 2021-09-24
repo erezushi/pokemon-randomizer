ba1646b258aa07dae1602e579b2a7aaf99342111
Erez Bracha <erezushi900@gmail.com>
2021-09-24 12:55:26 +0300

    Removed unnecessary file and field from Pokémon type

--- src/constants.ts
+++
@@ -1,4 +0,0 @@
-export const POKEMON_FILE = 'data/pokemon.json';
-export const TYPES_FILE = 'data/types.json';
-
-export const DEFAULT_NUMBER = 6;
--- src/types.ts
+++ src/types.ts
@@ -33,7 +33,6 @@ export interface Pokemon {
     mythical?: boolean,
     basic?: boolean,
     forms?: Form[],
-    modifiedForms?: Form[],
 }
 
 export interface PokemonMap {
