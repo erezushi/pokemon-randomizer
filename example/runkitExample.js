691413689409da1a835623f428d96c35c7bb00e0
Erez Bracha <erezushi900@gmail.com>
2023-01-25 20:07:34 +0200

    Typo fix in RunKit example

--- example/runkitExample.js
+++ example/runkitExample.js
@@ -1,5 +1,6 @@
-// Default export is messed up on requires, import statements can be done normally
-const { deafult: randomPokemon } = require('@erezushi/pokemon-randomizer');
+// Default export is messed up on requires,
+// import statements can be done normally
+const { default: randomPokemon } = require('@erezushi/pokemon-randomizer');
 
 const options = {
     number: 3,
